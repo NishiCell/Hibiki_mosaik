74d55d40474ff79280093d83c4c9590beeeb158f
NishiCell <celluloce24@gmail.com>
2017-03-20 16:47:48 +0900

    innerHTML=""を使用 　>ボタンで文章を表示する 　>【index.html】ボタンを追加 　>【zzz.js】ボタンで文章を表示する関数を追加

--- zzz.js
+++ zzz.js
@@ -5,3 +5,8 @@ document.getElementById("B2").addEventListener("click", function(){
 document.getElementById("B1").addEventListener("click", function(){
         document.getElementById("target").style.visibility="visible";
 });
+
+
+document.getElementById("pushit").addEventListener("click", function(){
+        document.getElementById("about").innerHTML="消せます";
+});
